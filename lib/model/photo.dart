a6db31cd6f0a2e35a8907600e15ee6bb5dd1d439
wogks <wogks27@naver.com>
2022-11-23 01:33:01 +0900

    Feat: freezed

--- lib/model/photo.dart
+++ lib/model/photo.dart
@@ -1,100 +1,16 @@
-class Photo {
-  Photo({
-    required this.id,
-    required this.pageURL,
-    required this.type,
-    required this.tags,
-    required this.previewURL,
-    required this.previewWidth,
-    required this.previewHeight,
-    required this.webformatURL,
-    required this.webformatWidth,
-    required this.webformatHeight,
-    required this.largeImageURL,
-    required this.imageWidth,
-    required this.imageHeight,
-    required this.imageSize,
-    required this.views,
-    required this.downloads,
-    required this.collections,
-    required this.likes,
-    required this.comments,
-    required this.userId,
-    required this.user,
-    required this.userImageURL,
-  });
-  late final int id;
-  late final String pageURL;
-  late final String type;
-  late final String tags;
-  late final String previewURL;
-  late final int previewWidth;
-  late final int previewHeight;
-  late final String webformatURL;
-  late final int webformatWidth;
-  late final int webformatHeight;
-  late final String largeImageURL;
-  late final int imageWidth;
-  late final int imageHeight;
-  late final int imageSize;
-  late final int views;
-  late final int downloads;
-  late final int collections;
-  late final int likes;
-  late final int comments;
-  late final int userId;
-  late final String user;
-  late final String userImageURL;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  Photo.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    pageURL = json['pageURL'];
-    type = json['type'];
-    tags = json['tags'];
-    previewURL = json['previewURL'];
-    previewWidth = json['previewWidth'];
-    previewHeight = json['previewHeight'];
-    webformatURL = json['webformatURL'];
-    webformatWidth = json['webformatWidth'];
-    webformatHeight = json['webformatHeight'];
-    largeImageURL = json['largeImageURL'];
-    imageWidth = json['imageWidth'];
-    imageHeight = json['imageHeight'];
-    imageSize = json['imageSize'];
-    views = json['views'];
-    downloads = json['downloads'];
-    collections = json['collections'];
-    likes = json['likes'];
-    comments = json['comments'];
-    userId = json['user_id'];
-    user = json['user'];
-    userImageURL = json['userImageURL'];
-  }
+part 'photo.freezed.dart';
 
-  Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['id'] = id;
-    data['pageURL'] = pageURL;
-    data['type'] = type;
-    data['tags'] = tags;
-    data['previewURL'] = previewURL;
-    data['previewWidth'] = previewWidth;
-    data['previewHeight'] = previewHeight;
-    data['webformatURL'] = webformatURL;
-    data['webformatWidth'] = webformatWidth;
-    data['webformatHeight'] = webformatHeight;
-    data['largeImageURL'] = largeImageURL;
-    data['imageWidth'] = imageWidth;
-    data['imageHeight'] = imageHeight;
-    data['imageSize'] = imageSize;
-    data['views'] = views;
-    data['downloads'] = downloads;
-    data['collections'] = collections;
-    data['likes'] = likes;
-    data['comments'] = comments;
-    data['user_id'] = userId;
-    data['user'] = user;
-    data['userImageURL'] = userImageURL;
-    return data;
-  }
+part 'photo.g.dart';
+
+@freezed
+class Photo with _$Photo {
+  const factory Photo({
+    required int id,
+    required String tags,
+    required String previewURL,
+  }) = _Photo;
+
+  factory Photo.fromJson(Map<String, Object?> json) => _$PhotoFromJson(json);
 }
